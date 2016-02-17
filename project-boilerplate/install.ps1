0eca133facdd6a38a44ff6664af77012cb883241
Jakub Pawłowski <pawlowskijakub@gmail.com>
2016-02-17 12:50:01 +0100

    more universal 'install-modulelink' function

--- project-boilerplate/install.ps1
+++ project-boilerplate/install.ps1
@@ -1,13 +1,24 @@
 [CmdletBinding(SupportsShouldProcess=$true)]
 param($srcDir = ".", [switch][bool] $importonly) 
 
-function install-modulelink($modulename) {
-    $path = "C:\Program Files\WindowsPowershell\Modules\$modulename"
-    $target = "$PSScriptRoot\..\$srcDir\$modulename"
+function install-modulelink {
+    [CmdletBinding(SupportsShouldProcess=$true)]
+    param([Parameter(mandatory=$true)][string]$modulename) 
+    
+    
+    $target = $modulename
+    if ($target.EndsWith(".psm1")) {
+        $target = split-path -parent ((get-item $target).FullName)    
+    }
     $target = (get-item $target).FullName
-
+    
+    $modulename = split-path -leaf $target
+    $path = "C:\Program Files\WindowsPowershell\Modules\$modulename"
     if (test-path $path) {
         if ($PSCmdlet.ShouldProcess("removing path $path")) {
+            # packagemanagement module may be locking some files in existing module dir
+            if (gmo powershellget) { rmo powershellget }
+            if (gmo packagemanagement) { rmo packagemanagement }
             remove-item -Recurse $path
         }
     }
@@ -23,7 +34,7 @@ if (!$importonly) {
     
     if ($IsAdmin) {    
         $root = $psscriptroot
-        $modules = get-childitem "$root\..\$srcDir" -filter "*.psm1" -recurse | % { $_.Directory.Name }
+        $modules = get-childitem "$root\..\$srcDir" -filter "*.psm1" -recurse | % { $_.Directory.FullName }
         $modules | % { install-modulelink $_ }
     } else {
         Invoke-Elevated .\install.ps1 @PSBoundParameters
