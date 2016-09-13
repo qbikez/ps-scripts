f6f85088fd3e94f722de380704b2d28eb0a7de41
jakub.pawlowski <pawlowskijakub@gmail.com>
2016-09-13 09:05:27 +0200

    use nuget v2 to publish modules. use psgallery as default source

--- push.ps1
+++ push.ps1
@@ -5,60 +5,94 @@ function push-module {
 [CmdletBinding(SupportsShouldProcess=$true)]
 param($modulepath, [switch][bool]$newversion, [switch][bool]$newbuild, $version, $buildno, $source, $apikey)
 	write-verbose "publishing module from dir $modulepath"
-	
-    if ($source -ne $null) {
-        $repo = $source
-    } 
-    else {
-        $repo = "$env:PS_PUBLISH_REPO"
+	    
+    $nugeturl = "https://dist.nuget.org/win-x86-commandline/v2.8.6/nuget.exe"
+    if (!(test-path "$psscriptroot/.tools/nuget.exe")) {
+        if (!(test-path "$psscriptroot/.tools")) { $null = mkdir "$psscriptroot/.tools" }
+        # use nuget2, because nuget3 can cause errors when pushing modules (nuget.exe : '' is not a valid version string. At C:\Program Files\WindowsPowerShell\Modules\PowerShellGet\1.0.0.1\PSModule.psm1:6784 char:19)
+        invoke-webrequest $nugeturl -OutFile "$psscriptroot/.tools/nuget.exe"
     }
-    if ($apikey -ne $null) {
-        $key = $apikey
-    } else {
-        $key = "$env:PS_PUBLISH_REPO_KEY"
-    }
-    . $psscriptroot\imports\set-moduleversion.ps1
-    . $psscriptroot\imports\nuspec-tools.ps1
+    $_path = $env:path
+    try {
+        ipmo pathutils
+        Add-ToPath "$psscriptroot/.tools/nuget.exe" -first
 
-    $ver = get-moduleversion $modulepath
-    write-verbose "detected module version: $ver"
-    if ($newversion) {
-        $newver = Increment-Version $ver
-    } else {
-        $newver = $ver
-    }
-    if ($version -ne $null) {
-        $newver = $version
-    }
+        if ($source -ne $null) {
+            $repo = $source
+        } 
+        else {
+            $repo = "$env:PS_PUBLISH_REPO"
+        }
+        if ([string]::IsNullOrEmpty($repo)) {
+            $repo = "PSGallery"
+        }
+
+        if ([string]::IsNullOrEmpty($repo)) {
+            throw "no repository given and no PS_PUBLISH_REPO env variable set"
+        }
+
+        if ($apikey -ne $null) {
+            $key = $apikey
+        } else {
+            $key = "$env:PS_PUBLISH_REPO_KEY"
+        }
+        if ([string]::IsNullOrEmpty($key)) {
+            try {
+                $key = get-passwordcached $repo
+            } catch {
+            }
+        }
+
+        if ([string]::IsNullOrEmpty($key)) {
+            throw "no apikey given, no PS_PUBLISH_REPO_KEY env variable set and no cached password for repo $repo found"
+        }
+
+        . $psscriptroot\imports\set-moduleversion.ps1
+        . $psscriptroot\imports\nuspec-tools.ps1
+
+        $ver = get-moduleversion $modulepath
+        write-verbose "detected module version: $ver"
+        if ($newversion) {
+            $newver = Increment-Version $ver
+        } else {
+            $newver = $ver
+        }
+        if ($version -ne $null) {
+            $newver = $version
+        }
     
-    if ($buildno -ne $null -or $newbuild) { 
-        $splits = $newver.split(".")
-        $lastbuild = 0
-        if ($splits.length -gt 3) {
-            $newver = [string]::Join(".", ($splits | select -First 3))
-            $lastbuild = [int]::Parse($splits[3])            
-        }        
-        if ($newbuild) { $buildno = $lastbuild + 1 }
-        $newver += ".$buildno"
-     }
+        if ($buildno -ne $null -or $newbuild) { 
+            $splits = $newver.split(".")
+            $lastbuild = 0
+            if ($splits.length -gt 3) {
+                $newver = [string]::Join(".", ($splits | select -First 3))
+                $lastbuild = [int]::Parse($splits[3])            
+            }        
+            if ($newbuild) { $buildno = $lastbuild + 1 }
+            $newver += ".$buildno"
+         }
 
-    write-verbose "new module version: $newver"
-    set-moduleversion $modulepath -version $newver
+        write-verbose "new module version: $newver"
+        set-moduleversion $modulepath -version $newver
 
-    write-verbose "publishing module version: $newver"
+        write-verbose "publishing module version: $newver"
 
-    import-module PowerShellGet
-    import-module PackageManagement
+        import-module PowerShellGet
+        import-module PackageManagement
     
-    if (!(Get-PSRepository $repo -ErrorAction Continue)) {
-        write-host "registering PSRepository $repo"
-        Register-PSRepository -Name $repo -SourceLocation $repo/nuget -PublishLocation $repo -InstallationPolicy Trusted -Verbose
+        if (!(Get-PSRepository $repo -ErrorAction Continue)) {
+            write-host "registering PSRepository $repo"
+            Register-PSRepository -Name $repo -SourceLocation $repo/nuget -PublishLocation $repo -InstallationPolicy Trusted -Verbose
+        }
+        $repourl = & git remote get-url origin 
+        write-host "publishing module $modulepath v$newver to repo $repo. projecturi=$repourl"
+    
+        if ($pscmdlet.ShouldProcess("publishing module $modulepath v$newver to repo $repo")) {
+            Publish-Module -Path $modulepath -Repository $repo -NuGetApiKey $key -Verbose
+        }
     }
-    $repourl = & git remote get-url origin 
-    write-host "publishing module $modulepath v$newver to repo $repo. projecturi=$repourl"
-    
-    if ($pscmdlet.ShouldProcess("publishing module $modulepath v$newver to repo $repo")) {
-        Publish-Module -Path $modulepath -Repository $repo -NuGetApiKey $key -Verbose
+    finally {
+        $env:path = $_path
     }
 }
 
