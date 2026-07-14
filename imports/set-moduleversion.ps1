8022e25151333a1ed463d6144ce1e34b6a142299
jakub.pawlowski <jakub.pawlowski@guestline.com>
2026-07-14 08:05:21 +0200

    better spaces handling

--- imports/set-moduleversion.ps1
+++ imports/set-moduleversion.ps1
@@ -1,64 +1,66 @@
-function find-ModulePsd([parameter(mandatory=$true)]$path) {
+function find-ModulePsd([parameter(mandatory = $true)]$path) {
     if ($path.EndsWith(".psd1")) {
         $psd = $path
     }
     elseif ($path.EndsWith(".psm1")) {
-        $psd = $path -replace ".psm1",".psd1"
+        $psd = $path -replace ".psm1", ".psd1"
     }
     else {
-        $modulename = split-path -leaf $path
+        $modulename = Split-Path -Leaf $path
         $psd = "$path\$modulename.psd1"
     }
-    if (!(test-path $psd)) {
-        $psd = get-childitem $path -Filter "*.psd1"
+    if (!(Test-Path $psd)) {
+        $psd = Get-ChildItem $path -Filter "*.psd1"
         if ($null -ne $psd) {
             $psd = $psd.FullName
             $modulename = [System.IO.Path]::GetFileNameWithoutExtension($psd)            
         }
     }
-    if (!(test-path $psd)) {
+    if (!(Test-Path $psd)) {
         throw "psd1 file '$psd' not found in path '$path'"
     }
 
-    return $psd,$modulename
+    return $psd, $modulename
 
 }
 
 function Set-ModuleVersion {
-    [CmdletBinding(SupportsShouldProcess=$true)]
+    [CmdletBinding(SupportsShouldProcess = $true)]
     param(
-        [parameter(mandatory=$true)]
+        [parameter(mandatory = $true)]
         $path,
-        [parameter(mandatory=$true)]
+        [parameter(mandatory = $true)]
         [string]$version
     ) 
 
-   $psd,$modulename = find-modulepsd $path
+    $psd, $modulename = find-modulepsd $path
 
     $c = Get-Content $psd | Out-String 
-    if ($c -match "ModuleVersion\s=\s'(.+)'") {
-        write-host "replacing version $($Matches[1]) with $version in $psd"
+    if ($c -match "ModuleVersion\s*=\s*'(.+)'") {
+        Write-Host "replacing version $($Matches[1]) with $version in $psd"
     }
-    $c = $c -replace "ModuleVersion\s=\s'.+'","ModuleVersion = '$version'" 
+    else {
+        throw "ModuleVersion not found in $psd"
+    }
+    $c = $c -replace "ModuleVersion\s*=\s*'.+'", "ModuleVersion = '$version'" 
     if ($PSCmdlet.ShouldProcess("save '$psd' with new version '$version'")) {
-        $c | Out-File $psd -encoding utf8
+        $c | Out-File $psd -Encoding utf8
     }
 }
 
 function Get-ModuleVersion(
-    [parameter(mandatory=$true)]
+    [parameter(mandatory = $true)]
     $path
-    ) 
-{
-    $psd,$modulename = find-modulepsd $path
+) {
+    $psd, $modulename = find-modulepsd $path
     
     if (!$psd) {
         throw "psd1 file '$psd' not found in path '$path'"
     }
     $c = Get-Content $psd
     $versionLine = $c | Where-Object { $_ -notmatch "^\s*#" } `
-        | Where-Object { $_ -match "ModuleVersion\s*=\s*'(.+)'" } `
-        | Select-Object -First 1
+    | Where-Object { $_ -match "ModuleVersion\s*=\s*'(.+)'" } `
+    | Select-Object -First 1
     if (!$versionLine) {
         throw "ModuleVersion not found in $psd"
     }
